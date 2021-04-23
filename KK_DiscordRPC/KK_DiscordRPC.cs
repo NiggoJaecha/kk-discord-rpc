6c27e303d379f231091d7b4edc6a9f76b7721cc6
NiggoJaecha <andre.grabowich@gmail.com>
2021-04-23 16:41:16 +0200

    Update and Time
    Added automatic updating of the RPC
    Added displaying of the elapsed time scince the game was started.

--- KK_DiscordRPC/KK_DiscordRPC.cs
+++ KK_DiscordRPC/KK_DiscordRPC.cs
@@ -2,18 +2,23 @@
 using BepInEx;
 using BepInEx.Logging;
 using KKAPI;
+using UnityEngine;
 
 namespace KK_DiscordRPC
 {
-    [BepInPlugin(GUID, "Koikatsu: Discord Rich Presence", Version)]
+    [BepInPlugin(GUID, "KK_DiscordRPC", Version)]
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
     public class Koi_DiscordRPC : BaseUnityPlugin
     {
-        public const string GUID = "varkaria.DiscordRPC";
+        public const string GUID = "varkaria.njaecha.DiscordRPC";
         public const string Version = "1.0";
 
         internal static DiscordRPC.RichPresence prsnc;
         internal new static ManualLogSource Logger;
+
+        private float CheckInterval;
+        private float CoolDown;
+        public long startstamp;
 
         private void Awake()
         {
@@ -26,30 +31,46 @@ namespace KK_DiscordRPC
                 false,
                 "643270");
 
+            startstamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            CheckStatus();
+            Logger.LogInfo("Discord Rich Presense Started");
+        }
+        private void Update()
+        {
+            if (CoolDown > 0)
+                CoolDown -= Time.deltaTime;
+            else
+            {
+                CoolDown = CheckInterval;
+                CheckStatus();
+            }
+        }
+        void CheckStatus()
+        {
             switch (KoikatuAPI.GetCurrentGameMode())
             {
                 case GameMode.Unknown:
-                    SetStatus("Koikatsu","Playing something", "studio", "Main game");
+                    SetStatus("Koikatsu", "In Main Menu", "main", "Main Menu");
                     break;
                 case GameMode.Maker:
-                    SetStatus("Koikatsu","Making a cute character", "main", "CharaStudio");
+                    SetStatus("Koikatsu", "In Charactermaker", "main", "CharacterMaker");
                     break;
                 case GameMode.Studio:
-                    SetStatus("CharaStudio","Making a picture", "studio", "Koikatsu");
+                    SetStatus("CharaStudio", "In Studio", "studio", "CharaStudio");
                     break;
                 case GameMode.MainGame:
-                    SetStatus("Koikatsu","Playing inside the main game", "studio", "Main game");
+                    SetStatus("Koikatsu", "In Game", "studio", "Main Game");
                     break;
                 default:
-                    SetStatus("Koikatsu","Playing something", "studio", "Main game");
+                    SetStatus("Koikatsu", "Playing something", "studio", "Main game");
                     break;
             }
         }
-        static void SetStatus(string mode, string status, string img, string img_text)
+        void SetStatus(string mode, string status, string img, string img_text)
         {
             prsnc.state = mode;
             prsnc.details = status;
-            prsnc.startTimestamp = 0;
+            prsnc.startTimestamp = startstamp;
             prsnc.largeImageKey = img;
             prsnc.largeImageText = img_text;
             prsnc.smallImageKey = null;
