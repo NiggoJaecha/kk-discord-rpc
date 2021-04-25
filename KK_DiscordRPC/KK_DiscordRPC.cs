b56a773189ba51d774fc8f4f13e3d92c42f3235f
NiggoJaecha <andre.grabowich@gmail.com>
2021-04-25 21:28:53 +0200

    Basic Studio Integration
    Added basic Studion Integration
    Added loaded mod and plugin keywords
    Added unknown gamemode config

--- KK_DiscordRPC/KK_DiscordRPC.cs
+++ KK_DiscordRPC/KK_DiscordRPC.cs
@@ -5,6 +5,7 @@ using BepInEx.Configuration;
 using KKAPI;
 using KKAPI.MainGame;
 using UnityEngine;
+using Sideloader;
 
 namespace KK_DiscordRPC
 {
@@ -25,9 +26,14 @@ namespace KK_DiscordRPC
         internal static long currentStamp;
         private GameMode old_Gamemode;
         private ChaFile old_Character;
+        private int loadedPluginsCount;
+        private int loadedModsCount;
+        private bool checkedCounts;
 
         private ConfigEntry<bool> configDisplayActivity;
         private ConfigEntry<string> configCustomActivityMessage;
+        private ConfigEntry<string> configCustomStateMessage;
+        private ConfigEntry<string> configCustomBigImage;
         private ConfigEntry<bool> configDisplayTime;
         private ConfigEntry<bool> configResetTime;
 
@@ -36,19 +42,25 @@ namespace KK_DiscordRPC
         private ConfigEntry<string> configBigImageMaker;
 
         private ConfigEntry<string> configBigImageStudio;
+        private ConfigEntry<string> configActivityMessageStudio;
+        private ConfigEntry<string> configStateMessageStudio;
 
         private ConfigEntry<string> configBigImageMainGame;
         private ConfigEntry<string> configActivityMessageMainGame;
         private ConfigEntry<string> configStateMessageMainGame;
 
+        private ConfigEntry<string> configBigImageUnknown;
+        private ConfigEntry<string> configActivityMessageUnknown;
+        private ConfigEntry<string> configStateMessageUnknown;
+
         private ConfigEntry<string> configBigImageHScene;
         private ConfigEntry<string> configActivityMessageHScene;
         private ConfigEntry<string> configStateMessageHScene;
         private ConfigEntry<string> configStateMessageHScene3P;
-        private ConfigEntry<string> configFreeHTrue;
-        private ConfigEntry<string> configFreeHFalse;
-        private ConfigEntry<string> configDarknessTrue;
-        private ConfigEntry<string> configDarknessFalse;
+        private ConfigEntry<string> hBoolFreeHTrue;
+        private ConfigEntry<string> hBoolFreeHFalse;
+        private ConfigEntry<string> hBoolDarknessTrue;
+        private ConfigEntry<string> hBoolDarknessFalse;
 
         private ConfigEntry<string> hModeAibzu;
         private ConfigEntry<string> hModeHoushi;
@@ -76,29 +88,37 @@ namespace KK_DiscordRPC
             AcceptableValueBase pictures = new AcceptableValueList<string>("logo_main", "logo_main_alt", "logo_studio", "sliders");
 
             configDisplayActivity = Config.Bind("_General_", "Display Activity", true, "Whether or not to display your activity (Ingame, Studio or Maker).");
-            configCustomActivityMessage = Config.Bind("_General_", "Custom Activity Message", "Ingame", "Message to be displayed when display Activity is turned off.");
+            configCustomBigImage = Config.Bind("_General_", "Image", "logo_main", new ConfigDescription("Displayed image when display Activity is turned off.", pictures));
+            configCustomActivityMessage = Config.Bind("_General_", "Custom Activity Message", "Ingame", "Activity message to be displayed when display Activity is turned off. Keywords:  <mod_count>, <plugin_count>");
+            configCustomStateMessage = Config.Bind("_General_", "Custom State Message", "<mod_count> Mods, <plugin_count> Plugins", "State message to be displayed when display Activity is turned off. Keywords: <mod_count>, <plugin_count>");
             configDisplayTime = Config.Bind("_General_", "Display Time", true, "Whether or not to display the elapsed time.");
             configResetTime = Config.Bind("_General_", "Reset Elapsed Time", true, "Whether or not to reset the elapsed time when chaning activity and/or loading diffrent character in maker.");
 
-            configActivityMessageMaker = Config.Bind("CharacterMaker", "Activity Message", "Maker (<maker_sex>)", "Activity message to display when in maker. Keywords: <maker_sex>");
-            configStateMessageMaker = Config.Bind("CharacterMaker", "State Message", "Editing: <chara_name>", "State message to display when editing a character in maker. Keywords: <chara_name>, <chara_nickname>");
             configBigImageMaker = Config.Bind("CharacterMaker", "Image", "sliders", new ConfigDescription("Displayed image when in Maker.", pictures));
+            configActivityMessageMaker = Config.Bind("CharacterMaker", "Activity Message", "Maker (<maker_sex>)", "Activity message to display when in maker. Keywords: <maker_sex>, <mod_count>, <plugin_count>");
+            configStateMessageMaker = Config.Bind("CharacterMaker", "State Message", "Editing: <chara_name>", "State message to display when editing a character in maker. Keywords: <chara_name>, <chara_nickname>, <mod_count>, <plugin_count>");
 
             configBigImageStudio = Config.Bind("CharaStudio", "Image", "logo_studio", new ConfigDescription("Displayed image when in CharaStudio.", pictures));
+            configActivityMessageStudio = Config.Bind("CharaStudio", "Activity Message", "In CharaStudio", "Activity Message to display when in Chara Studio. Keywords: <plugin_count>, <mod_count>");
+            configStateMessageStudio = Config.Bind("CharaStudio", "State Message", "<mod_count> Mods, <plugin_count> Plugins", "State Message to display when in Chara Studio. Keywords: <plugin_count>, <mod_count>");
 
             configBigImageMainGame = Config.Bind("Main Game", "Image", "logo_main", new ConfigDescription("Displayed image when in MainGame (everything but Maker and Studio).", pictures));
-            configActivityMessageMainGame = Config.Bind("Main Game", "Activity Message", "Ingame", "Actvity message to display when in main game but not H-Scene.");
-            configStateMessageMainGame = Config.Bind("Main Game", "State Message", "Menu/Roaming", "State message to display when in main game but not H-Scene.");
+            configActivityMessageMainGame = Config.Bind("Main Game", "Activity Message", "Ingame", "Actvity message to display when in main game but not H-Scene. Keywords: <plugin_count>, <mod_count>");
+            configStateMessageMainGame = Config.Bind("Main Game", "State Message", "<mod_count> Mods, <plugin_count> Plugins", "State message to display when in main game but not H-Scene. Keywords: <plugin_count>, <mod_count>");
+
+            configBigImageUnknown = Config.Bind("Loading/Unknown", "Image", "logo_main", new ConfigDescription("Displayed image when gamemode is unknown (usually when the game is still starting/loading).", pictures)); 
+            configActivityMessageUnknown = Config.Bind("Loading/Unknown", "Activity Message", "Starting Up", "Actvity message to display when gamemode is unknown (usually when the game is still starting/loading)");
+            configStateMessageUnknown = Config.Bind("Loading/Unknown", "State Message", "", "State message to display when gamemode is unknown (usually when the game is still starting/loading)");
 
             configBigImageHScene = Config.Bind("H-Scene", "Image", "logo_main_alt", new ConfigDescription("Displayed image when in H-Scene.", pictures));
-            configActivityMessageHScene = Config.Bind("H-Scene", "Activity Message", "H-Scene <hscene_type>", "Activity message to display when in H-Scene. Keywords: <is_freeH>, <is_darkness>, <heroine_name>, <heroine_nickname>, <hscene_type>");
-            configStateMessageHScene = Config.Bind("H-Scene", "State Message", "<hscene_mode> <heroine_name>", "State message to display when in H-Scene. Keywords: <hscene_mode>, <heroine_name>, <heroine_nickname>, <heroine_experience>, <heroine_safety>");
-            configStateMessageHScene3P = Config.Bind("H-Scene", "State Message Two Girls", "<hscene_mode> <heroine_name> and <secondary_name>", "State message to display when in H-Scene with two Girls. Keywords: <hscene_mode>, <heroine_name>, <heroine_nickname>, <heroine_experience>, <heroine_safety>, <secondary_name>, <secondary_nickname>, <secondary_experience>, <secondary_safety> ");
+            configActivityMessageHScene = Config.Bind("H-Scene", "Activity Message", "H-Scene <hscene_type>", "Activity message to display when in H-Scene. Keywords: <is_freeH>, <is_darkness>, <heroine_name>, <heroine_nickname>, <hscene_type>, <plugin_count>, <mod_count>");
+            configStateMessageHScene = Config.Bind("H-Scene", "State Message", "<hscene_mode> <heroine_name>", "State message to display when in H-Scene. Keywords: <hscene_mode>, <heroine_name>, <heroine_nickname>, <heroine_experience>, <heroine_safety>, <plugin_count>, <mod_count>");
+            configStateMessageHScene3P = Config.Bind("H-Scene", "State Message Two Girls", "<hscene_mode> <heroine_name> and <secondary_name>", "State message to display when in H-Scene with two Girls. Keywords: <hscene_mode>, <heroine_name>, <heroine_nickname>, <heroine_experience>, <heroine_safety>, <secondary_name>, <secondary_nickname>, <secondary_experience>, <secondary_safety>, <plugin_count>, <mod_count>");
 
-            configFreeHFalse = Config.Bind("H-Scene Keywords", "FreeH False Keywordvalue", "", "Value of <is_freeH> when NOT in FreeH-Mode");
-            configFreeHTrue = Config.Bind("H-Scene Keywords", "FreeH True Keywordvalue", "(Free H)", "Value of <is_freeH> when in FreeH-Mode");
-            configDarknessFalse = Config.Bind("H-Scene Keywords", "Darkness False Keywordvalue", "", "Value of <is_darkness> when NOT in a darkness scene");
-            configDarknessTrue = Config.Bind("H-Scene Keywords", "Darkness True Keywordvalue", "(Darkness)", "Value of <is_darkness> when in a darkness scene");
+            hBoolFreeHFalse = Config.Bind("H-Scene Keywords", "FreeH False Keywordvalue", "", "Value of <is_freeH> when NOT in FreeH-Mode");
+            hBoolFreeHTrue = Config.Bind("H-Scene Keywords", "FreeH True Keywordvalue", "(Free H)", "Value of <is_freeH> when in FreeH-Mode");
+            hBoolDarknessFalse = Config.Bind("H-Scene Keywords", "Darkness False Keywordvalue", "", "Value of <is_darkness> when NOT in a darkness scene");
+            hBoolDarknessTrue = Config.Bind("H-Scene Keywords", "Darkness True Keywordvalue", "(Darkness)", "Value of <is_darkness> when in a darkness scene");
 
             hModeAibzu = Config.Bind("H-Scene Keywords", "Mode Caress Keywordvalue", "Caressing", "Value of <hscene_mode> when carassing in normal H-Scene");
             hModeHoushi = Config.Bind("H-Scene Keywords", "Mode Service Keywordvalue", "Serviced by", "Value of <hscene_mode> when getting serviced in normal H-Scene");
@@ -110,7 +130,6 @@ namespace KK_DiscordRPC
             hModeSonyu3P = Config.Bind("H-Scene Keywords", "Mode Penetration Threesome Keywordvalue", "Doing", "Value of <hscene_mode> when penetrating in threesome H-Scene");
             hModeHoushi3PMMF = Config.Bind("H-Scene Keywords", "Mode Service MMF Keywordvalue", "Serviced by", "Value of <hscene_mode> when getting serviced in MMF (darkness) H-Scene");
             hModeSonyu3PMMF = Config.Bind("H-Scene Keywords", "Mode Penetration MMF Keywordvalue", "Doing", "Value of <hscene_mode> when penetrating in MMF (darkness) H-Scene");
-
             hTypeNormal = Config.Bind("H-Scene Keywords", "Type Normal Keywordvalue", "", "Value of <hscene_type> when in a normal H-Scene (Caress, Service, Penetration)");
             hTypeWatching = Config.Bind("H-Scene Keywords", "Type Watching Keywordvalue", "(Spectating)", "Value of <hscene_type> when in a H-Scene where the player is not involved");
             hTypeThreesome = Config.Bind("H-Scene Keywords", "Type Threesome Keywordvalue", "(Threesome)", "Value of <hscene_type> when in a threesome H-Scene (Threesome, Darkness)");
@@ -128,8 +147,18 @@ namespace KK_DiscordRPC
             old_Character = null;
             currentStamp = startStamp;
             CheckInterval = 3;
+            checkedCounts = false;
             CheckStatus();
             Logger.LogInfo("Discord Rich Presence started");
+        }
+        private void LateUpdate()
+        {
+            if (!checkedCounts)
+            {
+                loadedPluginsCount = BepInEx.Bootstrap.Chainloader.PluginInfos.Count;
+                loadedModsCount = Sideloader.Sideloader.ZipArchives.Count;
+                checkedCounts = true;
+            }
         }
         private void Update()
         {
@@ -148,7 +177,7 @@ namespace KK_DiscordRPC
                 switch (KoikatuAPI.GetCurrentGameMode())
                 {
                     case GameMode.Unknown:
-                        SetStatus("Unkown Gamemode", "Koikatsu", startStamp, "main", "Unknown");
+                        SetStatus(configActivityMessageUnknown.Value, configStateMessageUnknown.Value, startStamp, configBigImageUnknown.Value, "Loading");
                         old_Gamemode = KoikatuAPI.GetCurrentGameMode();
                         break;
                     case GameMode.Maker:
@@ -164,11 +193,11 @@ namespace KK_DiscordRPC
                         old_Gamemode = KoikatuAPI.GetCurrentGameMode();
                         break;
                     default:
-                        SetStatus("Playing something", "Koikatsu", startStamp, "logo_main", "Main game");
+                        SetStatus(configCustomActivityMessage.Value, configCustomStateMessage.Value, startStamp, configCustomBigImage.Value, "Koikatsu");
                         break;
                 }
             }
-            else SetStatus(null, configCustomActivityMessage.Value, startStamp, "logo_main", "Koikatsu");
+            else SetStatus(configCustomActivityMessage.Value, configCustomStateMessage.Value, startStamp, configCustomBigImage.Value, "Koikatsu");
         }
         void MakerStatus()
         {
@@ -177,7 +206,8 @@ namespace KK_DiscordRPC
             {
                 ChaFile character = KKAPI.Maker.MakerAPI.LastLoadedChaFile;
                 string activity = configActivityMessageMaker.Value;
-                string sex_string = "unknown";
+                string state = configStateMessageMaker.Value;
+                string sex_string = "";
                 Int32 sex = KKAPI.Maker.MakerAPI.GetMakerSex();
                 switch (sex) 
                 {
@@ -202,7 +232,10 @@ namespace KK_DiscordRPC
                 {
                     currentStamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
                 }
-                string state = configStateMessageMaker.Value;
+                activity = activity.Replace("<mod_count>", loadedModsCount.ToString());
+                activity = activity.Replace("<plugin_count>", loadedPluginsCount.ToString());
+                state = state.Replace("<mod_count>", loadedModsCount.ToString());
+                state = state.Replace("<plugin_count>", loadedPluginsCount.ToString());
                 state = state.Replace("<chara_name>", character.parameter.fullname);
                 state = state.Replace("<chara_nickname>", character.parameter.nickname);
                 SetStatus(activity, state, currentStamp, configBigImageMaker.Value, "CharacterMaker");
@@ -211,12 +244,28 @@ namespace KK_DiscordRPC
             }
             else
             {
-                SetStatus("In Charactermaker", "Koikatsu", startStamp, configBigImageMaker.Value, "CharacterMaker");
+                SetStatus(configActivityMessageUnknown.Value, configStateMessageUnknown.Value, startStamp, configBigImageUnknown.Value, "CharacterMaker");
             }
         }
         void StudioStatus()
         {
-            SetStatus("In Studio", "CharaStudio", startStamp, configBigImageStudio.Value, "CharaStudio");
+            
+            if (KKAPI.Studio.StudioAPI.StudioLoaded)
+            {
+                string activity = configActivityMessageStudio.Value;
+                string state = configStateMessageStudio.Value;
+
+                activity = activity.Replace("<mod_count>", loadedModsCount.ToString());
+                activity = activity.Replace("<plugin_count>", loadedPluginsCount.ToString());
+                state = state.Replace("<mod_count>", loadedModsCount.ToString());
+                state = state.Replace("<plugin_count>", loadedPluginsCount.ToString());
+
+                SetStatus(activity, state, startStamp, configBigImageStudio.Value, "CharaStudio");
+            }
+            else
+            {
+                SetStatus(configActivityMessageUnknown.Value, configStateMessageUnknown.Value, startStamp, configBigImageStudio.Value, "CharaStudio");
+            }
         }
         void MainGameStatus()
         {
@@ -286,19 +335,19 @@ namespace KK_DiscordRPC
                     switch (hproc.dataH.isFreeH)
                     {
                         case true:
-                            freeH = configFreeHTrue.Value;
+                            freeH = hBoolFreeHTrue.Value;
                             break;
                         case false:
-                            freeH = configFreeHFalse.Value;
+                            freeH = hBoolFreeHFalse.Value;
                             break;
                     }
                     switch (hproc.dataH.isDarkness)
                     {
                         case true:
-                            darkness = configDarknessTrue.Value;
+                            darkness = hBoolDarknessTrue.Value;
                             break;
                         case false:
-                            darkness = configDarknessFalse.Value;
+                            darkness = hBoolDarknessFalse.Value;
                             break;
                     }
                     switch (heroine.HExperience)
@@ -390,9 +439,12 @@ namespace KK_DiscordRPC
                         state = state.Replace("<secondary_safety>", secondarySafety);
                     }
                     state = state.Replace("<hscene_mode>", mode);
-
                 }
-            }   
+            }
+            activity = activity.Replace("<mod_count>", loadedModsCount.ToString());
+            activity = activity.Replace("<plugin_count>", loadedPluginsCount.ToString());
+            state = state.Replace("<mod_count>", loadedModsCount.ToString());
+            state = state.Replace("<plugin_count>", loadedPluginsCount.ToString());
             SetStatus(activity, state, currentStamp, image, "Main Game");
         }
 
@@ -412,17 +464,11 @@ namespace KK_DiscordRPC
             prsnc.partyMax = 0;
             DiscordRPC.UpdatePresence(ref prsnc);
         }
-        private void OnStartH(HSceneProc proc, bool freeH)
-        {
-            Logger.LogInfo("H-Scence started");
-            hproc = proc;
-        }
     }
     public class SceneGameController: GameCustomFunctionController
     {
         protected override void OnStartH(HSceneProc proc, bool freeH)
         {
-            Console.WriteLine("H Started");
             Koi_DiscordRPC.hproc = proc;
             Koi_DiscordRPC.currentStamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
         }
